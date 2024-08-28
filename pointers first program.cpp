ca35d4419e79f53b306ace931128087dbb4c771d
anushka2327 <anushkashinde0703@gmail.com>
2024-08-28 12:18:34 +0530

    Add files via upload

---
+++ pointers first program.cpp
@@ -0,0 +1,21 @@
+// Online c++ pointers
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a = 2;
+   int*aptr;
+   aptr=&a;
+   
+   cout<<&a<<endl;
+   cout<<aptr<<endl;
+   cout<<*aptr<<endl;
+   
+    return 0;
+}
+/*0x7fffc707db34
+0x7fffc707db34
+2
+
+
+=== Code Execution Successful ===/*
